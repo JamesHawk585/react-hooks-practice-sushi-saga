f6c7f556adf5ccba81363e8f35ad1e460b089c56
James Hawk <jph94880@gmail.com>
2023-10-23 18:54:13 -0500

    Uses state for sushi conveyor

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
 // SushiContainer serves as the "belt' in our sushi conveyor"
 
 function SushiContainer({sushis}) {
-  const sushisToDisplay = sushis.map(sushi => <Sushi key={sushi.id} sushi={sushi}/>)
+  const [currentSushiIndex, setCurrentSushiIndex] = useState(0)
+  const sushisToDisplay = sushis
+  .slice(currentSushiIndex, currentSushiIndex + 4)
+  .map(sushi => <Sushi key={sushi.id} sushi={sushi}/>)
+
+  function handleMoreSushi() {
+    setCurrentSushiIndex(currentSushiIndex => currentSushiIndex + 4)
+  }
 
 
   return (
     <div className="belt">
       {sushisToDisplay}
-      <MoreButton />
+      <MoreButton onMoreSushiClick={handleMoreSushi}/>
     </div>
   );
 }
